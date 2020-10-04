af8b6f12937bf7406a1a4dcafcface6a947ef0af
mkovacevic <kovacevicmilica544@gmail.com>
2020-10-04 21:07:29 +0200

    added search dropdown

--- src/views/dashboard/explore/explore.tsx
+++ src/views/dashboard/explore/explore.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Search from '../../../components/search-dropdown/search';
 import './styles.scss';
 
 const Explore = () => {
   return (
     <div className="p-flex p-flex-column">
       <div className="search-box p-flex p-justify-center p-items-center">
+        <Search />
       </div>
       <div className="destination-box p-flex p-justify-center">
         <h1>Destinations</h1>
