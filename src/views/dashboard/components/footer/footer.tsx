a3baffb44e776f95d5d7bf791dd15eedd62468f1
mkovacevic <mkovacevic@execom.eu>
2020-10-10 21:14:25 +0200

    fix

--- src/views/dashboard/components/footer/footer.tsx
+++ src/views/dashboard/components/footer/footer.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import "./footer.scss";
 
-const Footer = () => {
-    return <div className="p-flex p-justify-center">
+const Footer = () =>
+    <div className="p-flex p-justify-center">
         <h1>Footer</h1>
     </div>
-}
 
 export default Footer;
