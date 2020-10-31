82c7f2ff0c053017f53e58f31ae7406db4cbc7ad
mkovacevic <mkovacevic@execom.eu>
2020-10-31 17:41:15 +0100

    add top destination card

--- src/services/api/destination/destinationService.ts
+++ src/services/api/destination/destinationService.ts
@@ -3,3 +3,6 @@ import { get } from "../httpService";
 export const destinationUrl = () => "destinations";
 
 export const destinationRequest = async () => get<[]>(`${destinationUrl()}`);
+
+export const getTopDestinations = async () =>
+  get<[]>(`${destinationUrl()}/top`);
